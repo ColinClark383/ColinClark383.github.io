d826c2b037a804b718d6250b6676cc6932906dea
ColinClark383 <colin.clark@ufl.edu>
2025-01-03 00:36:50 -0500

    Setup Basic Layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Colin Clark</h1>
+        <p>innovation through code</p>
       </header>
+      <div className="App-todo">
+        <h3>to do for site</h3>
+        <ul>
+          <li>set up css design</li>
+          <li>set up departments of info(about, projects, contact, etc)</li>
+          <li>Figure out <i>actual</i> tagline</li>
+          <li>nav bar for easy navigation</li>
+          <li>have fun and learn!</li>
+        </ul>
+        </div>
     </div>
   );
 }
