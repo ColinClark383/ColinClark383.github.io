34e276eb3eb0faf925cfdc111d2c77dda4bc6a11
ColinClark383 <colin.clark@ufl.edu>
2025-01-16 18:45:09 -0500

    Added Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Navbar from './navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <h1>Colin Clark</h1>
-        <p>innovation through code</p>
+        <p>Modern computions, unique solutions</p>
       </header>
+      <main>
       <div className="App-todo">
         <h3>to do for site</h3>
         <ul>
@@ -14,9 +17,21 @@ function App() {
           <li>set up departments of info(about, projects, contact, etc)</li>
           <li>Figure out <i>actual</i> tagline</li>
           <li>nav bar for easy navigation</li>
+          <li>replace icons from react ones</li>
           <li>have fun and learn!</li>
         </ul>
         </div>
+        <div id="about">
+          <br/>
+          <p>this is where i would give a brief paragraph about myself</p>
+          <br/>
+        </div>
+        <div id="portfolio">
+          <br/>
+            <p>this is where i would talk about my projects</p>
+          <br/>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ my-app/src/navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+    return(
+        <div >
+        <nav role="navigation" className="navbar">
+            <ul className="navlist">
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    )
+}
